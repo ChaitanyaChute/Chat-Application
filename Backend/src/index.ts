bf5ace621ec2638b2987f02383d3931cb70ba150
Chaitanya-chute <chaitanyachute11@gmail.com>
2025-09-12 22:08:48 +0530

    Room Logic Added

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,11 +4,14 @@ const wss = new WebSocketServer({port:8080});
 
 interface customWebSocket extends WebSocket{
     username?:string;
+    currentRoom?:string | null ;
+
 }
 
 
 let userCount = 0 ;
 let users:customWebSocket[]= [];
+const rooms = new Map();
 
 
 
@@ -23,20 +26,62 @@ wss.on("connection",(ws)=>{
     console.log("connection established , TotalUsers :"+userCount);
 
     Customws.username = "anon";
+    Customws.currentRoom = null;
 
     Customws.on("message",(msg)=>{
         const data = JSON.parse(msg.toString())
 
         if(data.type == "username"){
-            Customws.username = data.username;
+            Customws.username = data.username || "anon";
+            
+            const outputData = JSON.stringify({
+                "type":data.type,
+                "payload":{
+                    "username":data.username 
+                }
+            })
 
-            Customws.send("Username set to : " + `${Customws.username}`);
+            Customws.send(outputData)
         }
 
         if(data.type == "join"){
-            
+            const room = data.room;
 
-            Customws.send("Username set to : " + `${Customws.username}`);
+            if(!rooms.has(room)){
+                rooms.set(room , new Set())
+            }
+
+            Customws.currentRoom = room ;
+
+            rooms.get(room).add(ws);
+
+
+            
+            const outputData = JSON.stringify(
+                {
+                    "type": data.type,
+                    "payload": {
+                        "roomId":data.room,
+                    }
+                }
+            )
+            
+            Customws.send(outputData);
+        }
+
+        if(data.type == "chat"){
+            const message = data.message;
+            
+            const outputData = JSON.stringify(
+                {
+                    "type": data.type,
+                    "payload": {
+                        "message":data.message
+                    }
+                }
+            )
+            
+            Customws.send(outputData);
         }
 
     })
